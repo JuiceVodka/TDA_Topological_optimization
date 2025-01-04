852699524d209ab58ceb46386c4b6850ceb594f3
burek <burek>
2025-01-04 12:12:37 +0100

    naive algorithm for adjusting points using bottleneck distance

--- proj.jl
+++ proj.jl
@@ -54,7 +54,7 @@ function influence_death(bar, cloud, epsilon = 0.1, direction = 1)
         #println(cloud[death_sx][i])
         #println("................")
     end
-    println(cloud)
+    # println(cloud)
 end
 
 
@@ -62,13 +62,13 @@ function influence_birth(bar, cloud, epsilon = 0.1, direction = 1)
     #direction -1 shortens the bar by making it die sooner, 1 lengthens the bar by making it die later
 
     birth_simplex = PersistenceDiagrams.birth_simplex(bar)
-    println(birth_simplex)
+    # println(birth_simplex)
     if length(birth_simplex) == 1
         #the birth simplex is a single pint in 1D homology, they all start at 0
         return
     end
     
-    print(cloud[birth_simplex])
+    # print(cloud[birth_simplex])
 
     simplices = cloud[birth_simplex]
 
@@ -86,7 +86,7 @@ function influence_birth(bar, cloud, epsilon = 0.1, direction = 1)
             avg = (avg[1] + vectors[j][1], avg[2] + vectors[j][2])
         end
         avg = (avg[1]/length(vectors), avg[2]/length(vectors))
-        println(avg)
+        # println(avg)
         cloud[findall(x -> x == simplices[i], cloud)[1]] = (simplices[i][1] - avg[1]*epsilon*direction, simplices[i][2] - avg[2]*epsilon*direction)
 
     end
@@ -95,23 +95,154 @@ function influence_birth(bar, cloud, epsilon = 0.1, direction = 1)
 end
 
 
+function circular_pointcloud(n, r)
+    cloud  = Tuple{Float64, Float64}[]
+    for i in 1:n
+        theta = 2*pi*rand()
+        push!(cloud, (r*cos(theta), r*sin(theta)))
+    end
+    return cloud
+end
+
+function circle_bounded_pointcloud(n, r)
+    cloud  = Tuple{Float64, Float64}[]
+    for i in 1:n
+        theta = 2*pi*rand()
+        R = r*rand()
+        push!(cloud, (R*cos(theta), R*sin(theta)))
+        
+    end
+    return cloud
+end
 
 
-cloud = random_cloud(10)
-#println(cloud)
-scatter(cloud)
-pd = bars(cloud)
-most_persistent = pd[2][end]
-#println(most_persistent)
-#println("test0")
-influence_death(most_persistent, cloud)
-
-#println("TEST")
-#println(cloud)
-scatter!(cloud)
-
-influence_birth(most_persistent, cloud)
-
-scatter!(cloud)
+# means birth and death are the same
+function is_diagonal(q)
+    return abs(q[1] - q[2]) < 1e-6  # Small tolerance for floating-point comparison
+end
 
 
+function adjust_points(A, B) 
+    a0, a1 = ripserer(A)
+    iterations = 10000
+    tolerance = 0.01
+    epsilon = 0.1
+    for i in 1:iterations
+        b0, b1 = ripserer(B)
+        
+        println("iteration $(i)")
+        # Compute matchings for both 0-dim and 1-dim barcodes
+        match_bottle_0 = matching(Bottleneck(), a0, b0)
+        match_bottle_1 = matching(Bottleneck(), a1, b1)
+
+        for (p, q) in matching(match_bottle_0)
+            
+            if (is_diagonal(p)) && (is_diagonal(q))
+                continue
+            end
+
+
+            # Compare births
+            if q.birth < p.birth
+                influence_birth(q, B, epsilon, 1)  # Delay birth
+            elseif q.birth > p.birth
+                influence_birth(q, B, epsilon, -1)  # Advance birth
+            end
+
+            # Compare deaths
+            if q.death < p.death
+                influence_death(q, B, epsilon, 1)  # Prolong death
+            elseif q.death > p.death
+                influence_death(q, B, epsilon, -1)  # Shorten death
+            end
+       
+        end
+
+        for (p, q) in matching(match_bottle_1)
+
+            if (is_diagonal(p)) && (is_diagonal(q))
+                continue
+            end
+
+            # Compare births
+            if q.birth < p.birth
+                influence_birth(q, B, epsilon, 1)  # Delay birth
+            elseif q.birth > p.birth
+                influence_birth(q, B, epsilon, -1)  # Advance birth
+            end
+
+            # Compare deaths
+            if q.death < p.death
+                influence_death(q, B, epsilon, 1)  # Prolong death
+            elseif q.death > p.death
+                influence_death(q, B, epsilon, -1)  # Shorten death
+            end
+        end
+
+        # # Compute new distance
+        # match_bottle_0 = matching(Bottleneck(), a0, b0)
+        loss = weight(match_bottle_0)
+        println("Current loss: $loss")
+    
+        # Stop if loss is below tolerance
+        if loss < tolerance
+            break
+        end
+    end
+end    
+
+
+r = 1
+n = 100
+A = circular_pointcloud(n, r)
+B = circle_bounded_pointcloud(n, r)
+
+scatter(A)
+scatter!(B)
+
+adjust_points(A, B)
+
+
+
+# iterations = 100
+# tolerance = 0.01
+# for _ in 1:iterations
+#     b0, b1 = ripserer(B)  
+
+#     match_bottle = matching(Bottleneck(), a0, b0)
+#     loss = distance(match_bottle)
+#     println("Current loss: $loss")
+
+#     # Stop if the loss is below the tolerance
+#     if loss < tolerance
+#         break
+#     end
+# end
+
+scatter!(B, label="B")
+
+
+
+
+
+
+# scatter(A)
+# scatter!(B)
+
+
+# cloud = random_cloud(10)
+# #println(cloud)
+# scatter(cloud)
+# pd = bars(cloud)
+# most_persistent = pd[2][end]
+# #println(most_persistent)
+# #println("test0")
+# influence_death(most_persistent, cloud)
+
+# #println("TEST")
+# #println(cloud)
+# scatter!(cloud)
+
+# influence_birth(most_persistent, cloud)
+
+# scatter!(cloud)
